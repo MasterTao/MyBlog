cca1d1c6f1e48caf9aae4f1e5e905dd76143cee2
txu1 <txu1@i9i8.com>
2021-04-16 18:04:44 +0800

    初始化框架

---
+++ src/main/java/com/monkey/blog/Application.java
@@ -0,0 +1,5 @@
+package com.monkey.blog;/**
+ * @author tao
+ * @date 2021/4/16 5:59 下午
+ */public class Application {
+}
---
+++ src/main/java/com/monkey/blog/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.monkey.blog.controller;/**
+ * @author tao
+ * @date 2021/4/16 6:02 下午
+ */public class UserController {
+}
