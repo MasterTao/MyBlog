1c338eb33460e9413807cbb1cf1bdc4466f9eb1d
txu1 <txu1@i9i8.com>
2021-04-19 15:12:12 +0800

    first commit

--- src/main/java/com/monkey/blog/Application.java
+++ src/main/java/com/monkey/blog/Application.java
@@ -1,5 +1,17 @@
-package com.monkey.blog;/**
+package com.monkey.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
  * @author tao
  * @date 2021/4/16 5:59 下午
- */public class Application {
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
--- src/main/java/com/monkey/blog/controller/UserController.java
+++ src/main/java/com/monkey/blog/controller/UserController.java
@@ -1,5 +1,18 @@
-package com.monkey.blog.controller;/**
+package com.monkey.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
  * @author tao
  * @date 2021/4/16 6:02 下午
- */public class UserController {
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
 }
